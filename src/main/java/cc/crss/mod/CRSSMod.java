ec3a633161d350cc22619d8f24307f853d3a0dfe
blryface <124838505+blryface@users.noreply.github.com>
2024-07-12 19:29:40 -0300

    fix: change the mod's enviroment.. thing

--- src/main/java/cc/crss/mod/CRSSMod.java
+++ src/main/java/cc/crss/mod/CRSSMod.java
@@ -1,15 +1,15 @@
 package cc.crss.mod;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CRSSMod implements ModInitializer {
+public class CRSSMod implements DedicatedServerModInitializer {
 
     public static final Logger LOGGER = LogManager.getLogger("crss");
 	@Override
-	public void onInitialize() {
+	public void onInitializeServer() {
 
 		LOGGER.info("Hello World!");
 	}
